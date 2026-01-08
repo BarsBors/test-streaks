aace21b1a18591fd7a43a809537dbde507968fe9
BarsBors <akyolbars47@gmail.com>
2026-01-08 23:57:48 +0300

    Fix PWA: manifest, service worker, icons, favicon
    Added manifest link + favicon
    
    Updated start_url/scope for GitHub Pages
    
    Improved service worker caching (no)
    
    Added maskable icons for install (useless)

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "streaks-v1";
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./streaks2.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request).then((res) => {
+        if (res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        }
+        return res;
+      });
+    })
+  );
+});
